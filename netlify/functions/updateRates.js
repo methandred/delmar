e4f3623124de097a6307d67458e5f3726c32c903
methandred <menshikov.do@gmail.com>
2024-09-06 16:14:55 +0300

    Update updateRates.js

--- netlify/functions/updateRates.js
+++ netlify/functions/updateRates.js
@@ -1,13 +1,3 @@
-let exchangeRates = {
-  usdBuy: "41.30",
-  usdSell: "41.45",
-  eurBuy: "45.70",
-  eurSell: "45.85",
-  usdEur: "1.110",
-  eurUsd: "1.103",
-  btcRate: "57551"
-};
-
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     try {
@@ -23,7 +13,7 @@ exports.handler = async (event, context) => {
       }
 
       // Сохранение данных
-      exchangeRates = { usdBuy, usdSell, eurBuy, eurSell, usdEur, eurUsd, btcRate };
+    let  exchangeRates = { usdBuy, usdSell, eurBuy, eurSell, usdEur, eurUsd, btcRate };
 
       return {
         statusCode: 200,
