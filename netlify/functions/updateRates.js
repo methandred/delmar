c5ee8865544fcc3ae7dcf19d7b3456b11a0bed95
methandred <menshikov.do@gmail.com>
2024-09-10 13:40:37 +0300

    Update updateRates.js

--- netlify/functions/updateRates.js
+++ netlify/functions/updateRates.js
@@ -1,20 +1,44 @@
-let exchangeRates = {
-  usdBuy: "41.30",
-  usdSell: "41.45",
-  eurBuy: "45.70",
-  eurSell: "45.85",
-  usdEur: "1.110",
-  eurUsd: "1.103",
-  btcRate: "57551"
+const axios = require('axios');
+
+const API_KEY = '$2a$10$ZFtFeEWyRLOasREJKNYfA.iQTosTAnOXLT9pp72YMA2rP7y5p0Rti'; // Укажите ваш API ключ JSONbin
+const BIN_ID = '66e02158e41b4d34e42cf746'; // Укажите ID вашего bin
+const BASE_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
+
+// Функция для получения данных из JSONbin
+const getRatesFromJSONbin = async () => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      headers: {
+        'X-Master-Key': API_KEY,
+      }
+    });
+    return response.data.record;
+  } catch (error) {
+    console.error('Error fetching data from JSONbin:', error);
+    return null;
+  }
+};
+
+// Функция для обновления данных в JSONbin
+const saveRatesToJSONbin = async (data) => {
+  try {
+    await axios.put(BASE_URL, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Master-Key': API_KEY,
+      }
+    });
+  } catch (error) {
+    console.error('Error saving data to JSONbin:', error);
+  }
 };
 
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     try {
       const data = JSON.parse(event.body);
-
-      // Валидация полученных данных
       const { usdBuy, usdSell, eurBuy, eurSell, usdEur, eurUsd, btcRate } = data;
+
       if (!usdBuy || !usdSell || !eurBuy || !eurSell || !usdEur || !eurUsd || !btcRate) {
         return {
           statusCode: 400,
@@ -22,8 +46,8 @@ exports.handler = async (event, context) => {
         };
       }
 
-      // Сохранение данных
-      exchangeRates = { usdBuy, usdSell, eurBuy, eurSell, usdEur, eurUsd, btcRate };
+      // Сохраняем полученные данные в JSONbin
+      await saveRatesToJSONbin({ usdBuy, usdSell, eurBuy, eurSell, usdEur, eurUsd, btcRate });
 
       return {
         statusCode: 200,
@@ -36,6 +60,7 @@ exports.handler = async (event, context) => {
       };
     }
   } else if (event.httpMethod === 'GET') {
+    const exchangeRates = await getRatesFromJSONbin();
     return {
       statusCode: 200,
       body: JSON.stringify(exchangeRates),
